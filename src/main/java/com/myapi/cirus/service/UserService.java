57eb5fb8f8dbf049d7e4d8a2993c240a3f7fabfa
CarolinaCedro <yotechgirl@gmail.com>
2022-08-12 15:29:32 -0300

    feat: update post

--- src/main/java/com/myapi/cirus/service/UserService.java
+++ src/main/java/com/myapi/cirus/service/UserService.java
@@ -1,6 +1,7 @@
 package com.myapi.cirus.service;
 
 import com.myapi.cirus.domain.User;
+import com.myapi.cirus.dto.UserDto;
 import com.myapi.cirus.repository.UserRepository;
 import com.myapi.cirus.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,23 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
+    //Controller busque todos
     public List<User>findAll(){
         return repo.findAll();
     }
 
+    //Controller busque por id
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    //Controller update por id
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDto objDto){
+        return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+    }
 }
