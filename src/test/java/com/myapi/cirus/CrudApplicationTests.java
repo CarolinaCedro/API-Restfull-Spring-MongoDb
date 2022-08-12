dce3ce93e82183b954f24b6a081b4bd5dfc0edb9
CarolinaCedro <yotechgirl@gmail.com>
2022-08-11 21:49:56 -0300

    feat: fisrt commit

---
+++ src/test/java/com/myapi/cirus/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapi.cirus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
