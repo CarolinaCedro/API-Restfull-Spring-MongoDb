79804a6af04f5c3db6e6c53fda073b8f4bed2953
CarolinaCedro <yotechgirl@gmail.com>
2022-08-12 14:50:08 -0300

    feat: pattern dto

--- src/main/java/com/myapi/cirus/resources/UserResource.java
+++ src/main/java/com/myapi/cirus/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.myapi.cirus.resources;
 
 import com.myapi.cirus.domain.User;
+import com.myapi.cirus.dto.UserDto;
 import com.myapi.cirus.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -20,8 +22,9 @@ public class UserResource {
     @Autowired
     private UserService service;
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDto>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
